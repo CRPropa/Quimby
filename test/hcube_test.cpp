1382e322794702860143d744cb5882f262242148
Gero Müller <unknown>
2016-09-01 12:37:08 +0200

    fix tests

--- test/hcube_test.cpp
+++ test/hcube_test.cpp
@@ -10,8 +10,9 @@ int main() {
 	float size = (db->getUpperBounds() - db->getLowerBounds()).length();
 
 	//hc.init(db, gadget.Vector3f(117200, 118600, 130500), 5000, 0.1, 0)
-	HCubeFile4::create(db, db->getLowerBounds(), size, 0.5, 1e-14, 2,
+	HCubeFile4::create(db.get(), db->getLowerBounds(), size, 0.5, 1e-14, 2, 2,
 			"test.hc4");
+
 
 	HCubeFile4 hf4("test.hc4");
 	const HCube4 *hc = hf4.hcube();
