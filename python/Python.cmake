59664d4d8877d8a5c467f7c91533391f7732d006
Gero Müller <gero.mueller@physik.rwth-aachen.de>
2016-12-16 02:10:55 +0100

    fix python detection

--- python/Python.cmake
+++ python/Python.cmake
@@ -17,6 +17,26 @@ IF(NOT PYTHON_H_FOUND)
 	MESSAGE(SEND_ERROR "Python.h not found")
 ENDIF()
 
+
+#find the site package destinaton 
+execute_process(
+	COMMAND ${PYTHON_EXECUTABLE} -c "import sys; from distutils import sysconfig; sys.stdout.write(sysconfig.get_python_lib(1,0,prefix='${CMAKE_INSTALL_PREFIX}'))"
+	OUTPUT_VARIABLE PYTHON_SITE_PACKAGES
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+execute_process(
+	COMMAND ${PYTHON_EXECUTABLE} -c "import sys; sys.stdout.write(str(sys.version_info[0]) + str(sys.version_info[1]))"
+	OUTPUT_VARIABLE PYTHON_VERSION
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+execute_process(
+	COMMAND ${PYTHON_EXECUTABLE} -c "import sys; sys.stdout.write(str(sys.version_info[0]) + str('.') + str(sys.version_info[1]))"
+	OUTPUT_VARIABLE PYTHON_DOT_VERSION
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
 # use python framework mechanism on apple for the libs
 IF(APPLE)
 	INCLUDE(CMakeFindFrameworks)
@@ -43,36 +63,34 @@ IF (MINGW)
 	)
 ENDIF(MINGW)
 
+
 IF(NOT APPLE AND NOT MSVC AND NOT MINGW)
-	execute_process(
-		COMMAND ${PYTHON_EXECUTABLE} -c "import sys; from distutils import sysconfig; import os; libname = sysconfig.get_config_var('LDLIBRARY'); libdir= sysconfig.get_config_var('LIBDIR'); lib = os.path.join(libdir,libname); sys.stdout.write(lib)"
-		OUTPUT_VARIABLE PYTHON_LIBRARIES
-		OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
+  find_library(PYTHON_LIBRARY
+    NAMES
+    python${PYTHON_VERSION}
+    python${PYTHON_DOT_VERSION}mu
+    python${PYTHON_DOT_VERSION}m
+    python${PYTHON_DOT_VERSION}u
+    python${PYTHON_FOT_VERSION}
+    PATHS
+      [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\${_CURRENT_VERSION}\\InstallPath]/libs
+      [HKEY_CURRENT_USER\\SOFTWARE\\Python\\PythonCore\\${_CURRENT_VERSION}\\InstallPath]/libs
+    # Avoid finding the .dll in the PATH.  We want the .lib.
+    NO_SYSTEM_ENVIRONMENT_PATH
+  )
+  # Look for the static library in the Python config directory
+  find_library(PYTHON_LIBRARY
+    NAMES python${PYTHON_VERSION} python${PYTHON_DOT_VERSION}
+    # Avoid finding the .dll in the PATH.  We want the .lib.
+    NO_SYSTEM_ENVIRONMENT_PATH
+    # This is where the static library is usually located
+    PATH_SUFFIXES python${_CURRENT_VERSION}/config
+  )
 ENDIF(NOT APPLE AND NOT MSVC AND NOT MINGW)
-
-#find the site package destinaton 
-execute_process(
-	COMMAND ${PYTHON_EXECUTABLE} -c "import sys; from distutils import sysconfig; sys.stdout.write(sysconfig.get_python_lib(1,0,prefix='${CMAKE_INSTALL_PREFIX}'))"
-	OUTPUT_VARIABLE PYTHON_SITE_PACKAGES
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-execute_process(
-	COMMAND ${PYTHON_EXECUTABLE} -c "import sys; sys.stdout.write(str(sys.version_info[0]) + str(sys.version_info[1]))"
-	OUTPUT_VARIABLE PYTHON_VERSION
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-execute_process(
-	COMMAND ${PYTHON_EXECUTABLE} -c "import sys; sys.stdout.write(str(sys.version_info[0]) + str('.') + str(sys.version_info[1]))"
-	OUTPUT_VARIABLE PYTHON_DOT_VERSION
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
 
 MESSAGE(STATUS "Python: Found!")
 MESSAGE(STATUS "  Version:     " ${PYTHON_DOT_VERSION} "/" ${PYTHON_VERSION})
 MESSAGE(STATUS "  Executeable: " ${PYTHON_EXECUTABLE})
 MESSAGE(STATUS "  Include:     " ${PYTHON_INCLUDE_PATH})
-MESSAGE(STATUS "  Library:     " ${PYTHON_LIBRARIES})
+MESSAGE(STATUS "  Library:     " ${PYTHON_LIBRARY})
 MESSAGE(STATUS "  Site-package directory: " ${PYTHON_SITE_PACKAGES})
